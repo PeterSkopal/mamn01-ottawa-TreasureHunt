d1e4d079055f8602362ff1ce850446a12aa323f3
Peter Skopal <peter.skopal@hotmail.com>
2017-03-31 14:19:54 +0200

    Initial commit

---
+++ app/src/main/java/com/ottawa/treasurehunt/treasurehunt/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.ottawa.treasurehunt.treasurehunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
