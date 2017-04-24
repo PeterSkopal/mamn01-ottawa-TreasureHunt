8d9211d5d443bf7ee84ee5bd72c28cd1a3c0351b
Edvin Havic <edvinhavic@gmail.com>
2017-04-24 03:02:29 +0200

    Added maps activity

--- app/src/main/java/com/ottawa/treasurehunt/treasurehunt/MainActivity.java
+++ app/src/main/java/com/ottawa/treasurehunt/treasurehunt/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ottawa.treasurehunt.treasurehunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void onMapsClick (View v) {
+        Intent i = new Intent(this, MapsActivity.class);
+        this.startActivity(i);
+    }
+
+    public void onPlayClick (View v) {
+        Intent i = new Intent(this, Play.class);
+        this.startActivity(i);
     }
 }
